0dce221f532b2406d122d82dc4d5c8ad708b8be4
ValJeansEvilBread <64847510+ValJeansEvilBread@users.noreply.github.com>
2020-06-24 22:37:48 -0400

    first commit

---
+++ src/TicTacToeClass.java
@@ -0,0 +1,110 @@
+public class TicTacToeClass
+{
+	// Instance Variables
+	private char[][] board;
+	private int turns;
+
+	// Constructors
+	public TicTacToeClass()
+	{
+		board = new char[3][3];
+		turns = 0;
+
+		for ( int r=0; r<3; r++ ) {
+			for ( int c=0; c<3; c++ ) {
+				board[r][c] = ' ';
+			}
+		}
+	}
+
+	// Accessor Methods
+
+	public boolean isWinner( char p )
+	{
+		// top row
+		if(((board[0][0] == board[0][1]) && (board[0][1] == board[0][2])) && (board[0][2] == p)){
+		    return true;
+		// middle row
+		}else if(((board[1][0] == board[1][1]) && (board[1][1] == board[1][2])) && (board[1][2] == p)){
+		    return true;
+		// bottom row
+		}else if(((board[2][0] == board[2][1]) && (board[2][1] == board[2][2])) && (board[2][2] == p)){
+		    return true;
+		// left column
+		}else if(((board[0][0] == board[1][0]) && (board[1][0] == board[2][0])) && (board[2][0] == p)){
+		    return true;
+		// middle column
+		}else if(((board[0][1] == board[1][1]) && (board[1][1] == board[2][1])) && (board[2][1] == p)){
+		    return true;
+		// right column
+		}else if(((board[0][2] == board[1][2]) && (board[1][2] == board[2][2])) && (board[2][2] == p)){
+		    return true;
+		// back slash diagonal
+		}else if(((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) && (board[2][2] == p)){
+		    return true;
+		// forward slash diagonal
+		}else if(((board[0][2] == board[1][1]) && (board[1][1] == board[2][0])) && (board[2][0] == p)){
+		    return true;
+		// else false
+		}else {
+		return false;
+		}
+	}
+
+	public boolean isFull()
+	{
+		if (turns==9) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public boolean isCat()
+	{
+		if (isFull()==true && isWinner('X')==false && isWinner('O')==false) {
+			return true;
+		}else {
+		return false;
+		}
+	}
+
+	public boolean isValid( int r, int c )
+	{
+		if ( 0 <= r && r <= 2 && 0 <= c && c <= 2 )
+			return true;
+		else
+			return false;
+	}
+
+	public int numTurns()
+	{
+		return turns;
+	}
+
+	public char playerAt( int r, int c )
+	{
+		if ( isValid(r,c) )
+			return board[r][c];
+		else
+			return '@';
+	}
+
+	public void displayBoard()
+	{
+		System.out.println("  0  " + board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+		System.out.println("    --+-+--");
+		System.out.println("  1  " + board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+		System.out.println("    --+-+--");
+		System.out.println("  2  " + board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+		System.out.println("     0 1 2 ");
+	}
+
+	// Modifiers
+	public void playMove( char p, int r, int c )
+	{
+		board[r][c] = p;
+		turns++;
+	}
+
+}
